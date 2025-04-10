d6a4a556097a7f1eb597c23bb66258c7fabae72a
Adam Hajek <hejjack1@gmail.com>
2025-04-10 21:40:24 +0200

    rm __pycache__, add GPU training run scripts

---
+++ train_gpu.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Number of cores you want to use
+NUM_CORES=10
+# Which CPU cores to use (e.g., 0-3)
+CORE_RANGE="0-9"
+
+# Set thread limits
+export OMP_NUM_THREADS=$NUM_CORES
+export MKL_NUM_THREADS=$NUM_CORES
+export NUMEXPR_NUM_THREADS=$NUM_CORES
+export OPENBLAS_NUM_THREADS=$NUM_CORES
+export VECLIB_MAXIMUM_THREADS=$NUM_CORES
+
+# Run the training script with taskset
+CUDA_VISIBLE_DEVICES=0 taskset -c $CORE_RANGE python train.py --config configs/config260k.yaml
